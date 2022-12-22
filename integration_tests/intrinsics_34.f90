c942b32e6c6641ad173f8115ec3116098de5131f
Ondřej Čertík <ondrej@certik.us>
2022-12-22 09:42:35 -0700

    XX

--- integration_tests/intrinsics_34.f90
+++ integration_tests/intrinsics_34.f90
@@ -4,4 +4,9 @@ real :: x = 3.143
 real(dp) :: y = 2.33_dp
 print *, tiny(x), epsilon(x)
 print *, tiny(y), epsilon(y)
+!if (abs(tiny(x) - 1.17549435E-38) > 1e-
+
+!   1.17549435E-38   1.19209290E-07
+!   2.2250738585072014E-308   2.2204460492503131E-016
+
 end program
