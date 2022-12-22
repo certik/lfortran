ef693d73c5f4af25e409d44a9a35cb36eac020da
Ondřej Čertík <ondrej@certik.us>
2022-12-22 09:41:03 -0700

    Improve the epsilon/tiny test

--- integration_tests/intrinsics_34.f90
+++ integration_tests/intrinsics_34.f90
@@ -1,6 +1,7 @@
 program intrinsics_34
-    real :: x = 3.143
-    real(8) :: y = 2.33
-    print *, epsilon(x)
-    print *, epsilon(y)
+integer, parameter :: dp = kind(0.d0)
+real :: x = 3.143
+real(dp) :: y = 2.33_dp
+print *, tiny(x), epsilon(x)
+print *, tiny(y), epsilon(y)
 end program
