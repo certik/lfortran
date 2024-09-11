9a805acf786c56ba3309005ff835d5fc73a1ea5d
Ondřej Čertík <ondrej@certik.us>
2024-09-10 18:46:54 -0600

    Fix build1.sh

--- build1.sh
+++ build1.sh
@@ -1,16 +1,6 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
-    -DLFORTRAN_BUILD_ALL=yes \
-    -DWITH_STACKTRACE=yes \
-    -DWITH_RUNTIME_STACKTRACE=yes \
-    -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
-    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
-    -DCMAKE_INSTALL_LIBDIR=share/lfortran/lib \
+    -DCMAKE_PREFIX_PATH="$CONDA_PREFIX" \
     .
-cmake --build . -j16 --target install
+#cmake --build . -j16
