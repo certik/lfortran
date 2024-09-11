b52580edb9a0c62f7287002c805fd1f124ffdf85
Ondřej Čertík <ondrej@certik.us>
2024-09-10 18:53:07 -0600

    Update build script

--- build1.sh
+++ build1.sh
@@ -1,6 +1,7 @@
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
+    -DWITH_STACKTRACE=no \
     -DCMAKE_PREFIX_PATH="$CONDA_PREFIX" \
     .
-#cmake --build . -j16
+cmake --build . -j16
